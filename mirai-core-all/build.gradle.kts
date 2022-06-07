01c721a0456cb754164935204090fa2aac5e92d0
Karlatemp <kar@kasukusakura.com>
2022-06-07 23:50:33 +0800

    Remove platform-image in core-all

--- mirai-core-all/build.gradle.kts
+++ mirai-core-all/build.gradle.kts
@@ -23,7 +23,6 @@ dependencies {
     api(project(":mirai-core"))
     api(project(":mirai-core-api"))
     api(project(":mirai-core-utils"))
-    api(project(":mirai-core-utils-platform-image"))
 }
 
 if (System.getenv("MIRAI_IS_SNAPSHOTS_PUBLISHING")?.toBoolean() != true) {
